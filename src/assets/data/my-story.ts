915649ef2965c00bd513e5f117cd640f32d449b1
Tomas Baranek <Tomas.Baranek@contractors.twinformatics.at>
2023-01-13 13:58:42 +0100

    Added test data

---
+++ src/assets/data/my-story.ts
@@ -0,0 +1,71 @@
+export const myStory: any = {
+  sk: {
+    years: {
+      2017: {
+        company: 'Domov',
+        logo: 'Domov',
+        projects: {
+          0: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+          1: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+          2: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+        },
+      },
+      2016: {
+        company: 'Domov',
+        logo: 'Domov',
+        projects: {
+          0: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+        },
+      },
+      2015: {
+        company: 'Domov',
+        logo: 'Domov',
+        projects: {
+          0: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+          1: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+        },
+      },
+      2014: {
+        company: 'Domov',
+        logo: 'Domov',
+        projects: {
+          0: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+          1: {
+            title: 'ASASSASASSASASAS',
+            description: 'sasdsdadsdadadsa',
+            role: 'Developer',
+          },
+        },
+      },
+    },
+  },
+  en: {},
+};
--- src/assets/data/translate.ts
+++ src/assets/data/translate.ts
@@ -1,21 +1,27 @@
 export const translate: any = {
-    sk: {
-        menu: {
-            navigation1: "Domov",
-            navigation2: "Domov",
-            navigation3: "Domov",
-            navigation4: "Domov"
-        },
-        homePageContent: {
-            picture: {
-                name: "Tomas Baranek",
-                position: "Web Developer"
-            },
-            intro: {
-                title: "Intro",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacus ex, ullamcorper vel enim in, tincidunt mattis dolor. Nulla aliquet augue nunc, at volutpat diam pulvinar id. Praesent laoreet condimentum felis sit amet pulvinar. Vivamus viverra mauris erat, nec convallis velit iaculis sit amet. Praesent semper ante ut facilisis lacinia. Proin erat dui, volutpat sit amet arcu quis, condimentum euismod magna. Vestibulum sed arcu tincidunt, laoreet metus ac, venenatis arcu."
-            }
-        }
+  sk: {
+    menu: {
+      navigation1: 'Domov',
+      navigation2: 'Domov',
+      navigation3: 'Domov',
+      navigation4: 'Domov',
     },
-    en: {}
-}
+    homePageContent: {
+      picture: {
+        name: 'Tomas Baranek',
+        position: 'Web Developer',
+      },
+      intro: {
+        title: 'Intro',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacus ex, ullamcorper vel enim in, tincidunt mattis dolor. Nulla aliquet augue nunc, at volutpat diam pulvinar id. Praesent laoreet condimentum felis sit amet pulvinar. Vivamus viverra mauris erat, nec convallis velit iaculis sit amet. Praesent semper ante ut facilisis lacinia. Proin erat dui, volutpat sit amet arcu quis, condimentum euismod magna. Vestibulum sed arcu tincidunt, laoreet metus ac, venenatis arcu.',
+      },
+    },
+    secondPageContent: {
+      title: '',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacus ex, ullamcorper vel enim in, tincidunt mattis dolor. Nulla aliquet augue nunc, at volutpat diam pulvinar id. Praesent laoreet condimentum felis sit amet pulvinar. Vivamus viverra mauris erat, nec convallis velit iaculis sit amet. Praesent semper ante ut facilisis lacinia. Proin erat dui, volutpat sit amet arcu quis, condimentum euismod magna. Vestibulum sed arcu tincidunt, laoreet metus ac, venenatis arcu.',
+    },
+  },
+  en: {},
+};
